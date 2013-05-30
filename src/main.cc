9ecaf0700fd4f09b4ac85f7beef7b003d79f0456
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-05-30 14:45:36 +0900

    added basic structure for the widget to render.

--- src/main.cc
+++ src/main.cc
@@ -15,8 +15,80 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <string>
 #include <cstdlib>
 #include <gtk/gtk.h>
+#include <cairo/cairo.h>
+
+using namespace std;
+
+class ImageView {
+public:
+	ImageView(void);
+	virtual ~ImageView();
+	GtkWidget *get_widget(void);
+	void set_file(const string &path);
+private:
+	GtkWidget *m_widget;
+	size_t m_area_width, m_area_height;
+	
+	void connect_signals(void);
+
+	static gboolean
+	_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data);
+	static gboolean
+	_configure_event(GtkWidget *widget, GdkEvent *event, gpointer user_data);
+};
+
+ImageView::ImageView(void)
+: m_widget(NULL),
+  m_area_width(0),
+  m_area_height(0)
+{
+}
+
+ImageView::~ImageView(void)
+{
+}
+
+GtkWidget *ImageView::get_widget(void)
+{
+	if (!m_widget) {
+		m_widget = gtk_drawing_area_new();
+		connect_signals();
+	}
+	return m_widget;
+}
+
+void ImageView::set_file(const string &path)
+{
+}
+
+void ImageView::connect_signals(void)
+{
+	g_signal_connect(m_widget, "draw",
+	                 G_CALLBACK(_draw), this);
+	g_signal_connect(m_widget, "configure-event",
+	                 G_CALLBACK(_configure_event), this);
+}
+
+gboolean ImageView::_draw(GtkWidget *widget, cairo_t *cr,
+                          gpointer user_data)
+{
+	return TRUE;
+}
+
+gboolean ImageView::_configure_event(GtkWidget *widget, GdkEvent *event,
+                                     gpointer user_data)
+{
+	ImageView *obj = static_cast<ImageView *>(user_data);
+	GdkEventConfigure *configureEvt = (GdkEventConfigure *)event;
+	obj->m_area_width = configureEvt->width;
+	obj->m_area_height = configureEvt->height;
+	g_debug("Configure Event: %zdx%zd",
+	        obj->m_area_width, obj->m_area_height);
+	return FALSE;
+}
 
 static gboolean
 delete_event(GtkWidget *widget, GdkEvent *event, gpointer user_data) 
@@ -28,11 +100,18 @@ delete_event(GtkWidget *widget, GdkEvent *event, gpointer user_data)
 int main(int argc, char *argv[])
 {
 	gtk_init(&argc, &argv);
-	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_widget_show(window);
 
+	// make a window
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(window, "delete-event",
 	                 G_CALLBACK(delete_event), NULL);
+
+	ImageView image_view;
+	GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), image_view.get_widget(),
+	                   TRUE, TRUE, 0);
+	gtk_container_add(GTK_CONTAINER(window), hbox);
+	gtk_widget_show_all(window);
 	gtk_main();
 	return EXIT_SUCCESS;
 }
