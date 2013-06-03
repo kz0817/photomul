998697f72327dee71ce9ede1c16d86986f873424
Kazuhiro Yamato <kz0817@gmail.com>
2013-06-03 21:08:21 +0900

    added a new file: Utils.c

---
+++ src/Utils.cc
@@ -0,0 +1,30 @@
+/* photomul
+   Copyright (C) 2013 Kazuhiro Yamato
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Utils.h"
+
+namespace Utils {
+
+string get_path(GFile *gfile)
+{
+	char *path_cstr = g_file_get_path(gfile);
+	string path = path_cstr;
+	g_free(path_cstr);
+	return path;
+}
+
+} // namespace Utils
---
+++ src/Utils.h
@@ -0,0 +1,31 @@
+/* photomul
+   Copyright (C) 2013 Kazuhiro Yamato
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef Utils_h
+#define Utils_h
+
+#include <string>
+#include <gio/gio.h>
+
+using namespace std;
+
+namespace Utils
+{
+	string get_path(GFile *gfile);
+} // namespace Utils
+
+#endif // Utils_h
